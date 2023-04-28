faa378cfba6ba972e8fb1403e2d056731bbe31a8
Guilherme <guilhermemagno09@gmail.com>
2023-04-28 20:13:28 -0300

    adding multiple image_picker function

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -14,4 +18,16 @@ String getNameFromEmail(String email) {
   // result ['guilherme','@gmail.com']
   final nome = email.split('@')[0];
   return nome;
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imagesFiles = await picker.pickMultiImage();
+  if (imagesFiles.isNotEmpty) {
+    for (var image in imagesFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
