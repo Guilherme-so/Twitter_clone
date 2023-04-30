7d161956525864f3919d0bdb52802f4bfbe6cd28
Guilherme <guilhermemagno09@gmail.com>
2023-04-30 19:21:25 -0300

    custom like button woth animation

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,6 +1,8 @@
 import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:like_button/like_button.dart';
 import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/core/enums/tweet_type_enum.dart';
 import 'package:twitter_clone/features/auth/controller/auth.controller.dart';
@@ -102,10 +104,34 @@ class TweetCard extends ConsumerWidget {
                                   text: (tweet.reshareCount).toString(),
                                   onTap: () {},
                                 ),
-                                TweetIconButton(
-                                  pathName: AssetsConstants.likeOutlinedIcon,
-                                  text: (tweet.likes.length).toString(),
-                                  onTap: () {},
+                                LikeButton(
+                                  size: 25,
+                                  likeBuilder: (isLiked) {
+                                    return isLiked
+                                        ? SvgPicture.asset(
+                                            AssetsConstants.likeFilledIcon,
+                                            color: Pallete.redColor,
+                                          )
+                                        : SvgPicture.asset(
+                                            AssetsConstants.likeOutlinedIcon,
+                                            color: Pallete.greyColor,
+                                          );
+                                  },
+                                  likeCount: tweet.likes.length,
+                                  countBuilder: (likeCount, isLiked, text) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(left: 2.0),
+                                      child: Text(
+                                        text,
+                                        style: TextStyle(
+                                          color: isLiked
+                                              ? Pallete.redColor
+                                              : Pallete.whiteColor,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    );
+                                  },
                                 ),
                                 IconButton(
                                   onPressed: () {},
